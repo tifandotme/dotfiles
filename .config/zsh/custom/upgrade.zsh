71ede93fbea5017697be0e94578f71ab7c235ce0
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-02-06 17:37:28 +0700

    update

--- .config/zsh/custom/upgrade.zsh
+++ .config/zsh/custom/upgrade.zsh
@@ -1,1 +1,1 @@
-alias up="clear && sudo dnf up -y && flatpak update --noninteractive && bun upgrade"
+alias up="clear && sudo dnf up -y && flatpak update --noninteractive && bun upgrade && bun update --global"
