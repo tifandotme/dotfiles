1e7b2742ff44a8d84df7e2f45e12f3c8e3d313b7
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-05-21 12:06:21 +0700

    update

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -3,3 +3,4 @@ export BACKGROUND=0xd43c4a31 # same as borders's inactive_color
 export ACCENT=0xff9baca6     # same as borders's active_color
 export FOREGROUND=0xffe4e4ef
 export MUTED_FOREGROUND=0xd4afafaf
+export DANGER=0xffff7b7b
--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+source "$CONFIG_DIR/colors.sh"
+
 SSID=$(system_profiler SPAirPortDataType | awk '/Current Network Information:/ { getline; print substr($0, 13, (length($0) - 13)); exit }')
 
 if [ "$SSID" = "" ]; then
-  sketchybar --set "$NAME" icon="􀙈" label="Disconnected"
+  sketchybar --set "$NAME" \
+    icon="􀙈" \
+    label="" \
+    icon.color="${DANGER}" \
+    icon.padding_right=0
 else
-  sketchybar --set "$NAME" icon="􀙇" label="$SSID"
+  sketchybar --set "$NAME" \
+    icon="􀙇" \
+    label="$SSID" \
+    icon.color="${ACCENT}" \
+    icon.padding_right=6
 fi
