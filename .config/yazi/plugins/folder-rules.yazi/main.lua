e55403d6277f6cd340d91825edada5352bfd50cd
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-07-24 22:54:14 +0700

    update

--- .config/yazi/plugins/folder-rules.yazi/main.lua
+++ .config/yazi/plugins/folder-rules.yazi/main.lua
@@ -3,8 +3,10 @@ local function setup()
     local cwd = cx.active.current.cwd
     if cwd:ends_with("Downloads") then
       ya.manager_emit("sort", { "mtime", reverse = true, dir_first = true })
+      ya.manager_emit("linemode", { "mtime" })
     else
       ya.manager_emit("sort", { "extension", reverse = false, dir_first = true })
+      ya.manager_emit("linemode", { "none" })
     end
   end)
 end
