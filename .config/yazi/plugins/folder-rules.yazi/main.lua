30fdfb466bc5a14e5c6e1f06241a191fa75417d7
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-09-11 22:46:13 +0700

    update

--- .config/yazi/plugins/folder-rules.yazi/main.lua
+++ .config/yazi/plugins/folder-rules.yazi/main.lua
@@ -1,12 +1,12 @@
 local function setup()
   ps.sub("cd", function()
     local cwd = cx.active.current.cwd
-    if cwd:ends_with("Downloads") then
-      ya.manager_emit("sort", { "mtime", reverse = true, dir_first = true })
-      ya.manager_emit("linemode", { "mtime" })
+    if string.lower(tostring(cwd)):find("downloads") then
+      ya.emit("sort", { "mtime", reverse = true, dir_first = true })
+      ya.emit("linemode", { "mtime" })
     else
-      ya.manager_emit("sort", { "extension", reverse = false, dir_first = true })
-      ya.manager_emit("linemode", { "none" })
+      ya.emit("sort", { "extension", reverse = false, dir_first = true })
+      ya.emit("linemode", { "none" })
     end
   end)
 end
