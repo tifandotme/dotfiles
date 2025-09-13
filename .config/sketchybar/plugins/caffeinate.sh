e912c1034ebc1310acb8d5a46d96c231b173763a
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-09-13 16:46:23 +0700

    update

--- .config/sketchybar/plugins/caffeinate.sh
+++ .config/sketchybar/plugins/caffeinate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Set CONFIG_DIR with fallback if not already set
+CONFIG_DIR="${CONFIG_DIR:-$HOME/.config/sketchybar}"
 source "$CONFIG_DIR/colors.sh"
 
 CAFFINATE_ID=$(pmset -g assertions | grep "caffeinate" | awk '{print $2}' | cut -d '(' -f1 | head -n 1)
@@ -7,13 +9,9 @@ CAFFINATE_ID=$(pmset -g assertions | grep "caffeinate" | awk '{print $2}' | cut 
 # It was not a button click
 if [ -z "$BUTTON" ]; then
   if [ -z "$CAFFINATE_ID" ]; then
-    sketchybar --set "$NAME" icon="􂊭" \
-      background.color="${BACKGROUND}" \
-      icon.color="${ACCENT}"
+    sketchybar --set "$NAME" icon="􂊭"
   else
-    sketchybar --set "$NAME" icon="􂊭" \
-      background.color="${ACCENT}" \
-      icon.color="${BACKGROUND}"
+    sketchybar --set "$NAME" icon=""
   fi
   exit 0
 fi
@@ -21,12 +19,8 @@ fi
 # It is a mouse click
 if [ -z "$CAFFINATE_ID" ]; then
   caffeinate -id &
-  sketchybar --set "$NAME" icon="􂊭" \
-    background.color="${ACCENT}" \
-    icon.color="${BACKGROUND}"
+  sketchybar --set "$NAME" icon="􂊭"
 else
   kill -9 "$CAFFINATE_ID"
-  sketchybar --set "$NAME" icon="􂊭" \
-    background.color="${BACKGROUND}" \
-    icon.color="${ACCENT}"
+  sketchybar --set "$NAME" icon=""
 fi
