91fc2882c95176a65bc104ab68cfe251e5339f2a
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2026-06-09 13:50:59 +0700

    update

--- dot_config/sketchybar/plugins/executable_aerospace_mode.sh
+++ dot_config/sketchybar/plugins/executable_aerospace_mode.sh
@@ -3,13 +3,20 @@
 # shellcheck disable=SC1091
 source "$HOME/.config/theme/palette.sh"
 
-if [ "$SENDER" = "send_message" ]; then
-  if [ -n "$MESSAGE" ] && [ -n "$HOLD" ]; then
+if [[ "$SENDER" == "send_message" ]]; then
+  if [[ -n "${MESSAGE:-}" && -n "${HOLD:-}" ]]; then
     sketchybar --set "$NAME" \
       label="$MESSAGE" \
       drawing=on
+
+    if [[ "$HOLD" != "true" ]]; then
+      sleep 1
+      sketchybar --set "$NAME" \
+        drawing=off \
+        label=""
+    fi
   fi
-elif [ "$SENDER" = "hide_message" ]; then
+elif [[ "$SENDER" == "hide_message" ]]; then
   sketchybar --set "$NAME" \
     drawing=off \
     label=""
