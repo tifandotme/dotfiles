6223e6b4fd3d8ed84b3d6b82c0d025063af45f3f
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2026-07-13 15:45:09 +0700

    update

--- dot_config/sketchybar/plugins/executable_openusage.sh
+++ dot_config/sketchybar/plugins/executable_openusage.sh
@@ -18,9 +18,13 @@ usage="$(curl --connect-timeout 1 --max-time 2 --silent --show-error --fail "htt
 
 metric_data="$(jq -r --arg label "$metric" '
   .lines[] | select(.type == "progress" and .label == $label) |
-  (.periodDurationMs / 1000) as $duration |
-  (.resetsAt | sub("\\.[0-9]+Z$"; "Z") | fromdateiso8601) as $resets_at |
-  (($duration - ($resets_at - now)) / $duration * 100) as $pace |
+  if .resetsAt != null and .periodDurationMs != null then
+    (.periodDurationMs / 1000) as $duration |
+    (.resetsAt | sub("\\.[0-9]+Z$"; "Z") | fromdateiso8601) as $resets_at |
+    (($duration - ($resets_at - now)) / $duration * 100)
+  else
+    0
+  end as $pace |
   [.used, ([$pace, 0, 100] | sort | .[1])] | @tsv
 ' <<<"$usage")"
 IFS=$'\t' read -r used pace <<<"$metric_data"
